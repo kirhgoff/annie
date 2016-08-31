eeac765129d14e8bcc0bddc1c79b336f554fcb0f
Kirill Lastovirya <kirill.lastovirya@moex.com>
2016-08-31 18:29:31 +0300

    working on network creation

--- src/main/scala/org/kirhgoff/annie/NeuralNetworkImpl.scala
+++ src/main/scala/org/kirhgoff/annie/NeuralNetworkImpl.scala
@@ -2,11 +2,13 @@ package org.kirhgoff.annie
 
 import org.kirhgoff.annie.interfaces.{Network, Neuron, TransferFunction}
 
+import scala.util.Random
+
 /**
   * Created by kirilllastovirya on 31/08/2016.
   */
 
-object Sigmoid extends TransferFunction {
+class Sigmoid extends TransferFunction {
   override def function(x : Double) : Double = 1 / (1 + Math.pow(Math.E, -1 * x))
 
   override def derivative(x: Double) : Double = {
@@ -23,9 +25,8 @@ class NeuronImpl(
   extends Neuron {
 
   def output(inputs:List[Double]) : Double = {
-    val sum: Double = (inputs, weights).zipped
-      .map((input, weight) => input*weight).sum
-    val result: Double = sigmoid.function(bias + sum)
+    val sum = (inputs, weights).zipped.map((input, weight) => input*weight).sum
+    val result = sigmoid.function(bias + sum)
     println(s"Neuron $name inputs=$inputs result=$result")
     result
   }
@@ -36,7 +37,7 @@ class NeuralNetworkImpl(
   extends Network {
 
   override def calculate(inputs: List[Double]): Double = {
-    val result: List[Double] = layers.foldRight(inputs)(
+    val result = layers.foldRight(inputs)(
       (layer, outputs) => layer.map(neuron => neuron.output(outputs))
     )
     assert(result.length == 1)
@@ -45,7 +46,25 @@ class NeuralNetworkImpl(
 }
 
 object NetworkFactory {
+  val random = new Random()
+  val sigmoid = new Sigmoid
+
   def apply(inputs:Int, layers:List[Int]):Network = {
+    val size: Int = layers.head
+    layers.tail.foldRight(List(neuronList(inputs, size)))
+    ((count, neuronLists)=> )
+    null
     //layers.foldRight(List.tabulate())
   }
+
+  def neuronList(inputs: Int, count: Int): List[NeuronImpl] = {
+    List.tabulate(count)(_ => newNeuron(inputs))
+  }
+
+  def newNeuron(inputs: Int): NeuronImpl = {
+    new NeuronImpl("", randomDouble(), randomList(inputs), sigmoid)
+  }
+
+  def randomList(length:Int):List[Double] = List.tabulate(length)(_ => random.nextDouble())
+  def randomDouble() = random.nextDouble()
 }
